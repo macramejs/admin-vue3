04b241d63fb3620a9ed2fae3fdb6451755313db3
Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
2022-06-17 15:43:23 +0200

    wip

--- src/ui/Main/MainBody.vue
+++ src/ui/Main/MainBody.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex-1 pt-6 pb-10 px-8 overflow-y-auto"
+        class="flex-1 px-10 pt-6 pb-10 overflow-y-auto"
         :style="`height: calc(100vh - ${topPosition}px); top: ${topPosition}px;`"
     >
         <slot />
