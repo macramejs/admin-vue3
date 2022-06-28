912eb13ade50e4fc0291cb6431158e4e3194e2c5
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-06-28 12:05:20 +0200

    add save button with keyboard shortcut

---
+++ src/modules/save/index.ts
@@ -0,0 +1,2 @@
+export { default as SaveButton } from './SaveButton.vue';
+
--- src/pages/partials/layouts/components/PartialTopbarRight.vue
+++ src/pages/partials/layouts/components/PartialTopbarRight.vue
@@ -3,30 +3,13 @@
         <div class="flex items-center space-x-2">
             <SelectLocale />
         </div>
-        <Button @click="save()" :disabled="!partialForm.isDirty"> save </Button>
+        <SaveButton :disabled="!partialForm.isDirty" @save="partialForm.submit()"/>
     </div>
 </template>
 <script lang="ts" setup>
 import { SelectLocale } from '@/modules/localize';
 import { partialForm } from '@/entities';
 import { Button } from '@/ui';
+import { SaveButton } from '@/modules/save';
 
-const save = async () => {
-    await partialForm.submit();
-};
-
-const saveKeyboardShortcut = (callback: any) => {
-    document.addEventListener('keydown', (e) => {
-        let modifier = window.navigator.platform.match('Mac')
-            ? e.metaKey
-            : e.ctrlKey;
-
-        if (modifier && e.key === 's') {
-            e.preventDefault();
-            callback()
-        }
-    });
-};
-
-saveKeyboardShortcut(save);
 </script>
