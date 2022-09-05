f7d38f8893dc2f1b0a39f926d566b07cb3862d60
Lukas Heller <dev@lukas-heller.de>
2022-09-05 08:54:09 +0200

    add system users entity

---
+++ src/entities/system-users/system-user.form.ts
@@ -0,0 +1,28 @@
+import { useForm } from '@macramejs/macrame-vue3';
+import { loadSystemUser, updateOrCreateSystemUser } from './api';
+import { SystemUser, SystemUserForm } from '@/types';
+
+import { ref } from 'vue';
+
+export type UseSystemUserForm = () => SystemUserForm;
+
+export const SystemUserModel = ref<SystemUser>();
+
+// TODO: false type problem
+const useSystemUserForm = () => {
+    return useForm({
+        data: {
+            name: '',
+            email: '',
+            password: '',
+            district_association_id: 0,
+            is_admin: false,
+        },
+        submit: (data, id) => updateOrCreateSystemUser(data, id),
+        load: id => loadSystemUser(id),
+    });
+};
+
+const systemUserForm = useSystemUserForm();
+
+export { useSystemUserForm, systemUserForm };
