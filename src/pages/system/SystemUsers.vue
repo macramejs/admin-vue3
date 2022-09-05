3d248be6aa4b3a1ec9a00a359d7fe524df4405c9
Lukas Heller <dev@lukas-heller.de>
2022-09-05 09:21:04 +0200

    add system / system users index

---
+++ src/pages/system/SystemUsers.vue
@@ -0,0 +1,31 @@
+<template>
+    <Topbar>
+        <div>{{ $t('system.user_management') }}</div>
+        <AddSystemUserModal />
+    </Topbar>
+    <Main>
+        <MainBody>
+            <MainContent>
+                <SystemUsersIndex />
+            </MainContent>
+        </MainBody>
+    </Main>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { systemUserIndex } from '@/entities';
+import { Main, MainBody, MainContent } from '@/layout';
+import Topbar from '@/layout/components/Topbar.vue';
+import SystemUsersIndex from './components/SystemUsersIndex.vue';
+import AddSystemUserModal from './components/AddSystemUserModal.vue';
+import //
+'@/ui';
+
+const route = useRoute();
+
+onMounted(() => {
+    systemUserIndex.load();
+});
+</script>
---
+++ src/pages/system/lang.ts
@@ -0,0 +1,22 @@
+export const messages = {
+    de: {
+        system: {
+            system_management: 'Systemverwaltung',
+            user_management: 'Nutzerverwaltung',
+            help_text: '...',
+            new_user: 'User anlegen',
+            name: 'Name',
+            save: 'Speichern',
+        },
+    },
+    en: {
+        system: {
+            system_management: 'System Management',
+            user_management: 'User Management',
+            help_text: '...',
+            new_user: 'Add user',
+            name: 'Name',
+            save: 'Save',
+        },
+    },
+};
